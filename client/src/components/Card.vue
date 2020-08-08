c064b7f60026c93942d4a97493fd8974ec6f72c9
rudy <rudolf.zachar@gmail.com>
2020-08-08 08:39:43 +0100

    Card.vue template created

---
+++ client/src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <symbol></symbol>
+</template>
+
+<script>
+    import Symbol from '.Symbol.vue'
+export default {
+    name: 'card',
+    props: ['card'],
+    components: {
+      'symbol': Symbol
+    }
+
+}
+</script>
+
+
+<style>
+
+</style>
