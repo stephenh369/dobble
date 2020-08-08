fb5256aad766b2e183e2815b1bbbf312b0c215d5
Helena <inger.helena@gmail.com>
2020-08-08 10:49:46 +0100

    dealPlayerCard() & dealOpponentCard() functions

--- client/src/components/GameBoard.vue
+++ client/src/components/GameBoard.vue
@@ -10,13 +10,28 @@ export default {
     name: 'game-board',
     data() {
         return {
-            cards: []
+            cards: [],
+            playerCard: null,
+            opponentCard: null,
         }
     },
     mounted() {
         CardService.getCards()
         .then(cards => this.cards = cards);
-  }
+    },
+    methods: {
+        dealPlayerCard() {
+            const card = this.cards[Math.floor(Math.random() * this.cards.length)];
+            this.playerCard = card;
+        },
+        dealOpponentCard() {
+            const card = this.cards[Math.floor(Math.random() * this.cards.length)];
+            if (JSON.stringify(this.card) === JSON.stringify(this.cardOne) ) {
+                dealOpponentCard();
+            } else {
+                this.opponentCard = card;
+            }
+    }
 }
 </script>
 
