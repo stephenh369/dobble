4b20e5dfeed570c2d423acb08e2a1b2dfe167819
Stephen <stephen_h369@live.co.uk>
2020-08-09 15:07:24 +0100

    hover and active states for images

--- client/src/components/Symbol.vue
+++ client/src/components/Symbol.vue
@@ -1,10 +1,9 @@
 <template>
-  <div>
+    <div>
       <!-- <button class="btn-card" v-on:click="handleClick">{{ cardSymbol }}</button> -->
-      <img :src="getImgUrl(cardSymbol)" alt="symbol" v-on:click="handleClick"/>
+        <img :src="getImgUrl(cardSymbol)" alt="symbol" v-on:click="handleClick"/>
     </div>
 </template>
-
 <script>
 import { eventBus } from '@/main.js';
 
@@ -13,10 +12,10 @@ export default {
     props: ['cardSymbol'],
     methods: {
         handleClick() {
-            eventBus.$emit('symbol-selected', this.cardSymbol)
+            eventBus.$emit('symbol-selected', this.cardSymbol);
         },
         getImgUrl(symbol) {
-            return require('../assets/'+symbol+'.png')
+            return require('../assets/'+symbol+'.png');
         }
     }
 
@@ -24,8 +23,15 @@ export default {
 </script>
 
 <style scoped>
- img {
-     width: 20px;
-     height: 20px;
- }
+    img {
+        width: 20px;
+        height: 20px;
+        border-radius: 5px;
+    }
+    img:hover {
+        background-color: rgba(110, 110, 110, 0.2);
+    }
+    img:active {
+        background-color: rgba(117, 1, 117, 0.692);
+    }
 </style>
