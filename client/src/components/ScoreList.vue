972a5db44a0bd814fc1b16f500e2e255a1a8574c
Helena <inger.helena@gmail.com>
2020-08-10 15:07:03 +0100

    created ScoreList.vue

---
+++ client/src/components/ScoreList.vue
@@ -0,0 +1,25 @@
+<template>
+  
+</template>
+
+<script>
+import ScoreService from '../services/ScoreService';
+
+export default {
+    name: 'score-list',
+    data() {
+        return {
+            scores: []
+        }
+    },
+    mounted() {
+        ScoreService.getScores()
+            .then(scores => this.scores = scores);
+    }
+
+}
+</script>
+
+<style>
+
+</style>
