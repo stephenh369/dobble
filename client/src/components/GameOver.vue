fb94c9c4b66da0391f92ef57e43bd859d78d1f9c
Stephen <stephen_h369@live.co.uk>
2020-08-11 15:59:58 +0100

    game over screen styling, needs sizing and other minor tweaks

--- client/src/components/GameOver.vue
+++ client/src/components/GameOver.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="game-over">
+    <div id="game-over-container">
     <h1>Game Over</h1>
     <p>You scored: {{score}}</p>
     
     <form v-on:submit="submitScore" v-if="!scoreSubmitted">
       <label for="name-input">Enter your name to submit your high score</label>
-      <input type="text" id="name-input" v-model="name" required>
-      <input type="submit" value="Save">
+      <input type="text" id="name-input" placeholder="Enter name here..." v-model="name" required>
+      <input class="btn" type="submit" value="Save">
     </form>
 
     <p v-if="scoreSubmitted">Thanks, {{name}}! Your score has been saved.</p>
-    <button class="btn" v-on:click="restartGame">Restart?</button>
-    <button class="btn" v-on:click="mainMenu">Main menu</button>
-    <button class="btn" v-on:click="highScores">High scores</button>
+      <div id="game-over-btn-div">
+        <button class="btn btn-game-over" v-on:click="restartGame">Restart?</button>
+        <button class="btn btn-game-over" v-on:click="mainMenu">Main menu</button>
+        <button class="btn btn-game-over" v-on:click="highScores">High scores</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -67,6 +71,27 @@ export default {
 
 <style>
   #game-over {
-    background-color: white;
+    background-color: rgb(141, 83, 141);
+    width: 100%;
+    height: 100%;
+  }
+  #game-over-container {
+    position: absolute;
+    display: grid;
+    white-space: nowrap;
+    top: 45%;
+    left: 50%;
+    transform: translate(-50%, -45%);
+  }
+  #game-over-btn-div {
+    display: flex;
+    justify-content: start;
+    align-items: center;
+    width: 100%;
+  }
+  #name-input {
+    background: transparent;
+    border: none;
+    border-bottom: 2px solid black;
   }
 </style>
